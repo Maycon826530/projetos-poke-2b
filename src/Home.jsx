3534f72ab115d45cbc4269f09b2cf450d14dfbee
Maycon <moreiramaycon932@gmail.com>
2025-06-27 10:47:27 +0000

    flex grid

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,24 +1,16 @@
-import poke from "./assets/pokeapi_256.png";
-import eevee from './assets/eevee.jpg';
-import { Container, Row, Col, Image } from 'react-bootstrap';
-import './Home.css'; // Vamos usar isso para o fundo vermelho
-
-const images = [poke, eevee, poke, eevee, poke, eevee, poke, eevee, poke, eevee, poke, eevee, poke, eevee];
+import poke from "/assets/pokeapi_256.png";
+import eeve from './assets/eevee.jpg';
+import { Container, Row, Col, Image } from "react-bootstrap";
+import './Home.css'
+; 
 
 const Home = () => {
-  return (
-    <div className="home-background">
-      <Container className="py-4">
-        <Row className="g-3 justify-content-center">
-          {images.map((imgSrc, index) => (
-            <Col key={index} xs={6} sm={4} md={3} lg={2} className="d-flex justify-content-center">
-              <Image src={imgSrc} alt={`pokemon-${index}`} fluid rounded />
-            </Col>
-          ))}
-        </Row>
-      </Container>
+  return(
+    <div className="Home">
+      <h1> Home </h1>
+      <img src={poke} />
     </div>
-  );
-};
+  )
+}
 
 export default Home;
---
+++ src/PokeFlex.jsx
@@ -0,0 +1,8 @@
+import {Container, Row, Col, Image} from 'react-bootstrap'
+import './PokeFlex.css';
+
+const PokeFlex = () => {
+    return(
+        <div className=''
+    )
+}
